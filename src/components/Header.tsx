11724698b11b14a0496f0009b606c733f0756e6b
pemauric <pedro.a.mauricio1@gmail.com>
2023-10-20 23:49:22 -0300

    added initial components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const Header = (props: Props) => {
+    return (
+        <header>
+            <h1>React + Todo TS</h1>
+        </header>
+    )
+}
+
+export default Header
